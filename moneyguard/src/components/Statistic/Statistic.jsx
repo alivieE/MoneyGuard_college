67dbe559538831157c2bb24106c784d5729dd1b8
alivieE <153372860+alivieE@users.noreply.github.com>
2025-10-09 15:41:29 +0300

    sidebar setPage

--- moneyguard/src/components/Statistic/Statistic.jsx
+++ moneyguard/src/components/Statistic/Statistic.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Statistic = () => {
   return (
     <div>
-      Statictic
+      Statistic
     </div>
   )
 }
--- moneyguard/src/pages/Cabinet/SideBar.jsx
+++ moneyguard/src/pages/Cabinet/SideBar.jsx
@@ -1,24 +1,72 @@
-import React from 'react'
-import s from './SideBar.module.css'
-import { NavLink } from 'react-router-dom'
-import Image from '../../assets/index'
+import React, { useState, useEffect } from "react";
+import s from "./SideBar.module.css";
+import { NavLink } from "react-router-dom";
+import Image from "../../assets/index";
 
 const SideBar = () => {
+  const [page, setPage] = useState("home");
+
+  useEffect(() => {
+    try {
+      fetch('https://api.monobank.ua/bank/currency').then((data)=>{
+        return data.json()
+      }).then((res)=>{
+        console.log(res);
+        console.log(res.find((currency)=>{
+          return currency.currencyCodeA === 392
+        }));
+      })
+    } catch (error) {
+      
+    }
+  }, []);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div>
-        <div className={s.links}>
-            <div className={s.homeLink}>
-                <NavLink className={s.link} to='home'><img className={s.homeImage} src={Image.home}></img>Home</NavLink>
-            </div>
-            <div></div>
-            <NavLink className={s.link}to='statistic'><img className={s.statisticImage} src={Image.statistic}></img>Statistic</NavLink>
-        </div>
+      <div className={s.links}>
+        <div className={s.homeLink}>
+          <NavLink
+            onClick={() => {
+              setPage("home");
+            }}
+            className={s.link}
+            style={{ fontWeight: page === "home" ? "bold" : "normal" }}
+            to="home"
+          >
+            <div className="activeWrap">
+@@ -44,6 +45,7 @@
+            setPage("statistic");
+          }}
+          className={s.link}
+          style={{ fontWeight: page === "statistic" ? "bold" : "normal" }}
+          to="statistic"
+        >
+          <div className="activeWrap">
+@@ -54,10 +56,13 @@
+      </div>
       <div className={s.balance}>
         <p className={s.text}>YOUR BALANCE</p>
-        <p className={s.balanceValue}>₴ 24 000.00</p>
+        <div className={s.balanceCount}>
+          <p className={s.currency}>₴</p>
+          <p className={s.balanceValue}> 24 000.00</p>
+          </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
